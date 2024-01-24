d04566807180a57eafa7c7ec3421da1e9627f322
rodrigo bernardo medeiros <rodrimedeiros@gmail.com>
2024-01-24 11:52:16 -0300

    Create a class based view pointing do a HTML template

--- django-for-begginers/chapter_03/pages/urls.py
+++ django-for-begginers/chapter_03/pages/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from .views import HomePageView
 
-urlpatterns = [path("admin/", admin.site.urls), path("home/", include("pages.urls"))]
+urlpatterns = [
+    path("", view=HomePageView.as_view(), name="home"),
+]
